ee60d69429c91e659069cfc3883b6f02778f56c4
wataiPublic@gmail.com <wataiPublic@gmail.com>
2022-11-04 12:36:39 +0800

    111-1104-1

---
+++ js/javascript.js
@@ -0,0 +1,59 @@
+$(function(){
+    $(function(){
+        $(".jumpWindow .close,.jumpWindow .check").on("click",function(){
+            $(".filter")
+            .removeClass("display");
+        })
+    
+        $("footer ul li").on("click",function(){
+            var n = $(this).index();
+    
+            if(n == 0)
+            {
+                $(".filter,.jumpWindow.record")
+                .addClass("display")
+                .siblings().removeClass("display");
+            }
+            else if(n == 1)
+            {
+                $(".filter,.jumpWindow.rule")
+                .addClass("display")
+                .siblings().removeClass("display");
+            }
+        })
+    
+        $(".gameBox .tableBox button").on("click",function(){
+            $(".filter,.jumpWindow.betting")
+            .addClass("display")
+            .siblings().removeClass("display");
+        })
+    })
+})
+
+$(function(){
+    $(".jumpWindow .topList li").on("click",function(){
+        $(this)
+        .addClass("active")
+        .siblings().removeClass("active");
+    })
+
+    $(".jumpWindow .pageBox li").on("click",function(){
+        $(this)
+        .addClass("active")
+        .siblings().removeClass("active");
+    })
+
+    $(".rowBox").on("click",function(){
+        if($(this).hasClass("left"))
+        {
+            $(this)
+            .removeClass("left")
+            .addClass("right");
+        }
+        else{
+            $(this)
+            .removeClass("right")
+            .addClass("left");
+        }
+    })
+})
